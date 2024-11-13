9d44b815014595e2f9157e29590665ab7d5987bc
Kevin Le <kevin.k.le.2020@gmail.com>
2024-11-13 14:34:12 -0600

    now using actual rest api endpoints design principles

--- backend.py
+++ backend.py
@@ -1,9 +1,9 @@
 import io
-from typing import Union, NamedTuple, List
+from typing import Union, NamedTuple, List, Optional, Any
 from fastapi import (
-    FastAPI, File, UploadFile, 
-    HTTPException, Response, BackgroundTasks)
-from pydantic import BaseModel
+    FastAPI, File, UploadFile, Query,
+    HTTPException, Response, BackgroundTasks, Depends)
+from pydantic import BaseModel, model_validator
 import csv
 import pandas as pd
 import codecs
@@ -35,13 +35,36 @@ async def upload_csv(file: UploadFile = File(...)):
 
 
 # @app.get("/")
-# @app.post("/create_dashboard")
-# @app.post("/team6_callback")
-# @app.post("/{dashboard_id}")
-# @app.get("/dashboard_id_map")
-# @app.get("/get_table/{table_id}")
-# @app.get("/get_all_tables_and_columns")
-# @app.get("/table_id_map")
-# @app.get("/graph_id_map")
-# @app.get("/get_graph")
-# @app.post("/create_graph/{table_name}")
+# return map of all graph ids and their corresponding tables, axes, and info (if no parameters)
+class TableQuery(BaseModel):
+    table_id: Optional[str] = None
+    table_ids: Optional[List[str]] = None
+
+    @model_validator(mode='before')
+    def check_mutually_exclusive(cls, data: Any) -> Any:
+        table_id, table_ids = data.get('table_id'), data.get('table_ids')
+        if table_id and table_ids:
+            raise ValueError("'table_id' and 'table_ids' cannot be used together. Please provide only one.")
+        return data
+
+def parse_table_query(
+        table_id: Optional[str] = None,
+        table_ids: Optional[List[str]] = Query(None)
+) -> TableQuery:
+    return TableQuery(table_id=table_id, table_ids=table_ids)
+
+app.get("/tables")
+def get_tables(query: TableQuery = Depends(parse_table_query)):
+    if query.table_id:
+        return {'table': query.table_id}
+    elif query.table_ids:
+        return {'table': query.table_ids}
+    else:
+        return {'table': "lol"}
+# app.post("/tables")
+
+# app.get("/graphs") # return map of all graph ids and their corresponding tables, axes, and info (if no parameters)
+# app.post("/graphs")
+
+# @app.get("/dashboards")
+# @app.post("/dashboards")
