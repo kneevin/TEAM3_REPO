a59ef184069cdabc6b298f0325c2c0ef0b03275d
Kevin Le <kevin.k.le.2020@gmail.com>
2024-11-13 13:59:07 -0600

    moved data to folder, also deleted streamlit stuff

--- pages/integration-guide.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-from menu import menu
-
-st.title("Integration Guide")
-menu()
-
--- pages/view-dashboards.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-from menu import menu
-
-menu()
-st.title("View Dashboards")
-
